2d321023e5f7079e964241e9766dcfcca164cdf1
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-16 16:48:48 +1000

    add invalid in wish.rb, factory folder

--- spec/factories/wish.rb
+++ spec/factories/wish.rb
@@ -1,9 +1,15 @@
 FactoryBot.define do
   factory :wish do
-    title {'Test wish'}
+    sequence :title do |n|
+      "Factory Test Wish no.#{n}"
+    end
     description {'Test wish description'}
     is_anonymous {false}
     is_completed {false}
     is_matched {false}
+
+    wish :invalid do
+      title {nil}
+    end
   end
 end
