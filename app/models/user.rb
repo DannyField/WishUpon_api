b4983ab31922af60d31dbc9d2912f2e6ec298fa6
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-16 22:04:39 +1000

    all user spec tests passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ApplicationRecord
 
     has_secure_password
-    validates :email, presence: true
-    validates :email, uniqueness: true
-    # has_many :wishes
+    validates :email, presence: true, uniqueness: true
+    has_many :wishes
 
 end
