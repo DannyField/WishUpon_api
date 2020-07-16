d97145608022cba381976349185346c762e4de9b
DannyField <dannyfieldproductions@gmail.com>
2020-07-16 17:04:03 +1000

    tests for models, and controllers

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,8 @@
 class Keyword < ApplicationRecord
+
+    validates :word, presence: true;
+    # belongs_to :wish
+    # has_many :wish_keyword
+    # has_many :wishes, through: :wish_keywords
+
 end
