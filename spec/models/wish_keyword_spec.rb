da715b2217cdeacdd8c54161128a2c9c5fad4683
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-16 16:35:46 +1000

    add validation and association test for keyword model and wish_keyword

--- spec/models/wish_keyword_spec.rb
+++ spec/models/wish_keyword_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe WishKeyword, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'has a valid factory' do
+      expect(build(:wish_keyword)).to be_valid
+    end
+  end
+
+  context 'associations' do
+    it { expect(subject).to belong_to(:wish)}
+    it { expect(subject).to belong_to(:keyword)}
+  end
 end
