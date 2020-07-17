7eadc5860aeff3b82238c3d04937321a31fc6560
DannyField <dannyfieldproductions@gmail.com>
2020-07-17 23:49:00 +1000

    Creating users needed to have country_id included

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
 
     private
 
+    # When creating a new user, it seems that country_id must be included
     def user_params
-        params.require(:user).permit(:email, :password)
+        params.require(:user).permit(:email, :password, :first_name,:last_name,:nickname,:age,:is_admin,:gender,:country_id)
     end
 end
+
+
+
+        
