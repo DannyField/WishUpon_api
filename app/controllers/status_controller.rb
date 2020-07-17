ffa4052eecf52136fa345eb98567da3c66e2de16
DannyField <dannyfieldproductions@gmail.com>
2020-07-17 23:34:19 +1000

    getting jwt back from postman

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,5 +1,5 @@
 class StatusController < ApplicationController
-    # before_action :authenticate_user 
+    before_action :authenticate_user 
 
     def index 
         render json: { message: 'logged in' } 
