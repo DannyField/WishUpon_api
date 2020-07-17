1e52c9f03a465fb46a0c8e4a34f41eeda3a22955
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-17 12:18:38 +1000

    modified seed file, User, Wish, Keyword, Hobby, Country are able to be saved in db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,35 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# puts "Creating a user role"
+puts "Creating a user role"
 
-# 1.times do 
-#     u = User.create(
-#         email: "admin@wishupon.com",
-#         password_digest: "password",
-#         first_name: 'Adam',
-#         last_name: 'Admin',
-#         nickname: 'The Admin',
-#         age: 100,
-#         is_admin: true,
-#         gender: 0,
-#     )
-# puts "#{u.email} : was added to users"
-# end
+1.times do 
+    u = User.create(
+        email: "admin@wishupon.com",
+        password: "password",
+        first_name: 'Adam',
+        last_name: 'Admin',
+        nickname: 'The Admin',
+        age: 100,
+        is_admin: true,
+        gender: 0,
+        country_id: 1
+    )
+puts "#{u.email} : was added to users"
+end
 
-# puts "Creating new wishes"
+puts "Creating new wishes"
 
-# 1.times do
-#     w = Wish.create(
-#         title: "I want to win Lotto",
-#         description: "Need to pay off the mafia for my gambling debt. So please let me win Lotto",
-#         is_secret: false,
-#         is_anonymous: false,
-#         is_completed: false,
-#         is_matched: false,
-#         like: 4,
-#         expiry_time: nil,
-#     )
-# puts "#{w.title} : wish completed!"
-# end
+1.times do
+    w = Wish.create(
+        title: "I want to win Lotto",
+        description: "Need to pay off the mafia for my gambling debt. So please let me win Lotto",
+        is_secret: false,
+        is_anonymous: false,
+        is_completed: false,
+        is_matched: false,
+        like: 4,
+        expiry_time: nil,
+        user_id: 1
+    )
+puts "#{w.title} : wish completed!"
+end
 
+country1 = Country.create(name:"Australia")
+country2 = Country.create(name:"China")
+
+hobby1 = Hobby.create(name:"fishing")
+hobby2 = Hobby.create(name:"painting")
+
+keyword1 = Keyword.create(word:"friend")
+keyword2 = Keyword.create(word:"travel")
+keyword3 = Keyword.create(word:"japan")
+
+WishKeyword.create(wish_id:1, keyword_id:1)
+WishKeyword.create(wish_id:1, keyword_id:2)
+WishKeyword.create(wish_id:1, keyword_id:3)
+
+UserHobby.create(user_id: 1, hobby_id:2)
