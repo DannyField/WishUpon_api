b09f46ac2601a0ebec744d2ce38d5351016d174c
DannyField <dannyfieldproductions@gmail.com>
2020-07-17 22:02:58 +1000

    models and assoc tested

--- app/models/wish.rb
+++ app/models/wish.rb
@@ -4,6 +4,7 @@ class Wish < ApplicationRecord
     validates :description, presence: true, length: {in: 10..600}
     has_many :wish_keywords, dependent: :destroy
     has_many :keywords, through: :wish_keywords
+    has_many :comments
     belongs_to :user
 
 end
