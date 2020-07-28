031249a4a519919cb4b59dcf01780a3c7882436b
Danny Field <58800432+DannyField@users.noreply.github.com>
2020-07-28 14:28:26 +1000

    Updated fix for Heroku with the Root To
    Fix for the routes for Heroku

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,6 @@ Rails.application.routes.draw do
     resources :comments, only: [:index, :create, :destroy]
   end
   
-  # root to: 'wishes#index'
+  root to: 'wishes#index'
   # resources :users
 end
