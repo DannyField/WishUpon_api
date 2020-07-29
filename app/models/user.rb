714b7038ad01d5f196d9b3e0a4d5537bf2e1582a
Lanzhou Jiang <lzjiang99@gmail.com>
2020-07-29 13:17:15 +1000

    add hobbies controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
 
     has_secure_password
     validates :email, presence: true, uniqueness: true
+    validates :first_name, presence: true
     has_many :wishes
     has_many :comments
     has_many :user_hobbies, dependent: :destroy
