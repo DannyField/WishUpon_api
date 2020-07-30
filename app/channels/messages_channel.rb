a5611c3d2b3e1780717adcf6e22a4cfa643f9acc
DannyField <dannyfieldproductions@gmail.com>
2020-07-30 11:43:08 +1000

    added code comments to serializers

--- app/channels/messages_channel.rb
+++ app/channels/messages_channel.rb
@@ -1,8 +1,11 @@
 class MessagesChannel < ApplicationCable::Channel
+
+  # Will broadcast new messages added to a specific conversation
   def subscribed
     # stream_from "some_channel"
     # Sends the messages to the converstaion/room channel
     conversation = Conversation.find(params[:conversation])
+    # Expects an object from the model
     stream_for conversation
   end
 
